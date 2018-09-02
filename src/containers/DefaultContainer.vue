481162c6e6513d05c2bfcc1c5bf5f03d7bf729e6
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-09-02 00:09:55 -0300

    Header menus cleared

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -10,18 +10,11 @@
       <b-navbar-nav class="d-md-down-none">
         <b-nav-item class="px-3" to="/dashboard">Dashboard</b-nav-item>
         <b-nav-item class="px-3" to="/users" exact>Users</b-nav-item>
-        <b-nav-item class="px-3">Settings</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item class="d-md-down-none">
           <i class="icon-bell"></i>
-          <b-badge pill variant="danger">5</b-badge>
-        </b-nav-item>
-        <b-nav-item class="d-md-down-none">
-          <i class="icon-list"></i>
-        </b-nav-item>
-        <b-nav-item class="d-md-down-none">
-          <i class="icon-location-pin"></i>
+          <b-badge pill variant="danger"></b-badge>
         </b-nav-item>
         <DefaultHeaderDropdownAccnt/>
       </b-navbar-nav>
