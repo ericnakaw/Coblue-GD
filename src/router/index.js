09edfb4ea898329736eac805d2cf4cd2534ddc56
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-09-03 17:55:05 -0300

    - Adicionando rotas para as páginas de Login, 404 e 500
    - Ajuste da view do Login

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,11 @@ const Dashboard = () => import('@/views/Dashboard')
 const Users = () => import('@/views/users/Users')
 const User = () => import('@/views/users/User')
 
+// Views - Pages
+const Login = () => import('@/views/pages/Login')
+const Page404 = () => import('@/views/pages/Page404')
+const Page500 = () => import('@/views/pages/Page500')
+
 Vue.use(Router)
 
 export default new Router({
@@ -49,6 +54,31 @@ export default new Router({
               component: User,
             },
           ]
+        }
+      ]
+    },,
+    {
+      path: '/pages',
+      redirect: '/pages/404',
+      name: 'Pages',
+      component: {
+        render (c) { return c('router-view') }
+      },
+      children: [
+        {
+          path: '404',
+          name: 'Page404',
+          component: Page404
+        },
+        {
+          path: '500',
+          name: 'Page500',
+          component: Page500
+        },
+        {
+          path: 'login',
+          name: 'Login',
+          component: Login
         }
       ]
     }
